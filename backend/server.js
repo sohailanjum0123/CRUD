809d3c60ed2f82c3c82a884e273ca30d606194c6
Sohail <sohailanjum0123@gmail.com>
2023-04-15 16:52:46 +0500

    create api and connect with backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const cors = require("cors")
 require("./db/config");
-const User = require("./db/User");
+const User = require("./db/User")
 const app = express();
-
 app.use(express.json());
-app.post("/register",async(req, res)=>{
-    const user = new User(req.body);
-    const result = await user.save();
-    res.send(result)
+app.use(cors());
+app.post("/register",async (req,res)=>{
+    let user = new User(req.body);
+    let result = await user.save();
+    console.warn(result);
 })
 
-app.listen(5000);
+
+app.listen(5000)
